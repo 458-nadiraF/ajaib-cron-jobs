d283c63ade87dbe7f080177a97c9b2158867ad3a
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2025-05-13 20:39:01 +0700

    Update and rename trigger-bersiap.js to trigger-bersiap.cron.js

--- api/trigger-bersiap.cron.js
+++ api/trigger-bersiap.cron.js
@@ -1,3 +1,6 @@
+export const config = {
+  schedule: '55 12 * * 1-5', // 19:55 WIB = 12:55 UTC
+};
 export default async function handler(req, res) {
   const urls = ['https://puppeteer-render-zx37.onrender.com/bersiap'];
   await Promise.all(urls.map(url => fetch(url)));
